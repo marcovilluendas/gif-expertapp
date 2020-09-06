606f0e5b27c385b7f769c1188ca363a2abb7d9cd
mvilluendas <mvilluendas@hiberus.com>
2020-09-06 12:03:56 +0200

    Changes Build

---
+++ docsbuild/precache-manifest.d08c0508d20ab3a35b93bfe997e31ce1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618784412656edb88ff63fc0811bbc76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8af67e763b4632b1e5bd",
+    "url": "/static/css/main.489b97e5.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af67e763b4632b1e5bd",
+    "url": "/static/js/main.c246ea16.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
